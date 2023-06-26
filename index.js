bfcab3ebe73558dfc628ee74b8350109bf4b46cd
markbonchi <markbonchi@gmail.com>
2023-06-26 20:14:46 +0100

    API endpoint for time

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 // init project
 var express = require('express');
 var app = express();
+// var Port = 3000;
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -24,9 +25,24 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
-
+app.get("/api/:time", (req, res, next) => {
+  req.unix = Date.parse(req.params.time);
+  if (!req.unix) {
+    req.time = new Date(parseInt(req.params.time));
+    req.unix = req.params.time
+  } else {
+    req.time = new Date(req.unix)
+  }
+  next()
+}, (req, res) => {
+  res.json({ "unix": req.unix, "utc": req.time.toUTCString() })
+})
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log(`Your app is listening on port ${listener.address().port}`);
 });
+
+// var listener = app.listen(Port, function () {
+//   console.log(`Your app is listening on port ${Port}`);
+// });
